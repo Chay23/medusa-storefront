55e2dc6871cad59103921b8bf8ece13f1d46b916
Nazar Shcherbii <shcherbiynazar@gmail.com>
2025-01-20 16:57:53 +0200

    Add UI and icons libraries

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,40 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import { heroui } from '@heroui/react';
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+		'./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: 'var(--background)',
+				surface: 'var(--surface)',
+			},
+		},
+	},
+	plugins: [
+		heroui({
+			themes: {
+				light: {
+					colors: {
+						primary: {
+							DEFAULT: '#607D4A',
+						},
+					},
+				},
+				dark: {
+					colors: {
+						primary: {
+							foreground: '#FFFFFF',
+							DEFAULT: '#607D4A',
+						},
+					},
+				},
+			},
+		}),
+	],
 } satisfies Config;
