7e81c82f82051756a01fbb68b0d75f4347a3b758
Nazar Shcherbii <shcherbiynazar@gmail.com>
2025-01-28 18:35:36 +0200

    Add breadcrumbs to the collection pages

--- components/dashboard/collections/create/Form.tsx
+++ components/dashboard/collections/create/Form.tsx
@@ -7,6 +7,19 @@ import { useActionState, useEffect } from 'react';
 import { toast } from 'react-hot-toast';
 
 import { redirect } from 'next/navigation';
+import Breadcrumbs from '../../UI/breadcrumbs/Breadcrumbs';
+import { Breadcrumb } from '@/types/common/breadcrumbs';
+
+const breadcrumbs: Breadcrumb[] = [
+	{
+		title: 'Collections',
+		href: '/dashboard/collections',
+	},
+	{
+		title: 'Create',
+		href: '/dashboard/collections/create',
+	},
+];
 
 export default function CollectionCreate() {
 	const [actionState, formAction, isPending] = useActionState(
@@ -32,41 +45,44 @@ export default function CollectionCreate() {
 	}, [isPending]);
 
 	return (
-		<div className='content-container flex justify-center'>
-			<Form action={formAction} className='w-full max-w-[720px]'>
-				<div className='w-full flex justify-between'>
-					<div>
-						<h3 className='mb-2'>Create Collection</h3>
-						<p className='text-text-secondary'>
-							Create a new collection to organize your products.
-						</p>
-					</div>
-					<Button type='submit' color='primary'>
-						Create
-					</Button>
-				</div>
-				<div className='w-full flex gap-8 mt-8'>
-					<div className='flex-1 flex flex-col gap-1'>
-						<Input
-							name='title'
-							labelPlacement='outside'
-							label='Title'
-							placeholder='Collection Title'
-						/>
-						<div className='text-danger-400 text-xs'>
-							{actionState.errors.title ?? ''}
+		<>
+			<Breadcrumbs items={breadcrumbs} />
+			<div className='content-container flex justify-center'>
+				<Form action={formAction} className='w-full max-w-[720px]'>
+					<div className='w-full flex justify-between'>
+						<div>
+							<h3 className='mb-2'>Create Collection</h3>
+							<p className='text-text-secondary'>
+								Create a new collection to organize your products.
+							</p>
 						</div>
+						<Button type='submit' color='primary'>
+							Create
+						</Button>
 					</div>
-					<Input
-						name='handle'
-						labelPlacement='outside'
-						label='Handle'
-						placeholder='Collection Handle'
-						startContent={'/'}
-						className='flex-1'
-					/>
-				</div>
-			</Form>
-		</div>
+					<div className='w-full flex gap-8 mt-8'>
+						<div className='flex-1 flex flex-col gap-1'>
+							<Input
+								name='title'
+								labelPlacement='outside'
+								label='Title'
+								placeholder='Collection Title'
+							/>
+							<div className='text-danger-400 text-xs'>
+								{actionState.errors.title ?? ''}
+							</div>
+						</div>
+						<Input
+							name='handle'
+							labelPlacement='outside'
+							label='Handle'
+							placeholder='Collection Handle'
+							startContent={'/'}
+							className='flex-1'
+						/>
+					</div>
+				</Form>
+			</div>
+		</>
 	);
 }
--- components/dashboard/collections/list/Collections.tsx
+++ components/dashboard/collections/list/Collections.tsx
@@ -1,30 +1,44 @@
 'use client';
 
-import { Api } from '@/types/api';
+import type { Api } from '@/types/api';
+import type { Breadcrumb } from '@/types/common/breadcrumbs';
+
 import { Button, Link } from '@heroui/react';
 import CollectionsFilters from './Filters';
 import CollectionsTable from './Table';
+import Breadcrumbs from '../../UI/breadcrumbs/Breadcrumbs';
 
 type Props = {
 	collectionsRes: Api.AdminCollectionListResponse;
 };
 
+const breadcrumbs: Breadcrumb[] = [
+	{
+		title: 'Collections',
+		href: '/dashboard/collections',
+		active: true
+	},
+];
+
 export default function Collections({ collectionsRes }: Props) {
 	return (
-		<section className='content-container'>
-			<div className='flex justify-between mb-7'>
-				<h2>Collections</h2>
-				<Button
-					as={Link}
-					role='link'
-					href='/dashboard/collections/create'
-					color='primary'
-				>
-					Create Collection
-				</Button>
-			</div>
-			<CollectionsFilters />
-			<CollectionsTable collectionsRes={collectionsRes} />
-		</section>
+		<>
+			<Breadcrumbs items={breadcrumbs} />
+			<section className='content-container'>
+				<div className='flex justify-between mb-7'>
+					<h2>Collections</h2>
+					<Button
+						as={Link}
+						role='link'
+						href='/dashboard/collections/create'
+						color='primary'
+					>
+						Create Collection
+					</Button>
+				</div>
+				<CollectionsFilters />
+				<CollectionsTable collectionsRes={collectionsRes} />
+			</section>
+		</>
 	);
 }
