83c1b58e20497148b33a0d12a8d3640456890a11
Nazar Shcherbii <shcherbiynazar@gmail.com>
2025-05-01 13:46:16 +0300

    Preserve current searchparams when clicking on the same acitve nav link

--- components/dashboard/UI/sidebar/Navigation.tsx
+++ components/dashboard/UI/sidebar/Navigation.tsx
@@ -1,75 +1,12 @@
-import type { ReactNode } from 'react';
-
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import Inventory2OutlinedIcon from '@mui/icons-material/Inventory2Outlined';
-import CategoryOutlinedIcon from '@mui/icons-material/CategoryOutlined';
-import LocalMallOutlinedIcon from '@mui/icons-material/LocalMallOutlined';
-import GroupOutlinedIcon from '@mui/icons-material/GroupOutlined';
-import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
-import FeaturedPlayListIcon from '@mui/icons-material/FeaturedPlayList';
 import { Divider } from '@heroui/react';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 
-const sidebarItems: (
-	| { type: 'divider' }
-	| { type: 'item'; key: string; label: string; icon: ReactNode }
-)[] = [
-	{
-		type: 'item',
-		key: '/dashboard',
-		label: 'Dashboard',
-		icon: <HomeOutlinedIcon />,
-	},
-	{
-		type: 'item',
-		key: '/dashboard/products',
-		label: 'Products',
-		icon: <Inventory2OutlinedIcon />,
-	},
-	{
-		type: 'item',
-		key: '/dashboard/collections',
-		label: 'Collections',
-		icon: <FeaturedPlayListIcon />,
-	},
-	{
-		type: 'item',
-		key: '/dashboard/categories',
-		label: 'Catregories',
-		icon: <CategoryOutlinedIcon />,
-	},
-
-	{
-		type: 'item',
-		key: '/dashboard/orders',
-		label: 'Orders',
-		icon: <LocalMallOutlinedIcon />,
-	},
-	{
-		type: 'item',
-		key: '/dashboard/users',
-		label: 'Users',
-		icon: <GroupOutlinedIcon />,
-	},
-	{ type: 'divider' },
-	{
-		type: 'item',
-		key: '/dashboard/settings',
-		label: 'Settings',
-		icon: <SettingsOutlinedIcon />,
-	},
-];
-
-const isActive = (link: string, currPath: string) => {
-	if (link === '/dashboard') {
-		return link === currPath;
-	}
-	return currPath.startsWith(link);
-};
+import { usePathname, useSearchParams } from 'next/navigation';
+import { isActive, sidebarItems } from '@/lib/dashboard/sidebar/utils';
 
 export default function Navigation({}) {
 	const pathname = usePathname();
+	const searchParams = useSearchParams();
 
 	return (
 		<div className='flex flex-col gap-1'>
@@ -79,7 +16,11 @@ export default function Navigation({}) {
 				) : (
 					<Link
 						key={item.key}
-						href={item.key}
+						href={
+							isActive(item.key, pathname)
+								? `${item.key}?${searchParams.toString()}`
+								: item.key
+						}
 						className={`flex gap-5 items-center p-2 rounded-lg transition-all ease-in-out duration-300 hover:bg-primary-100 hover:text-primary ${
 							isActive(item.key, pathname) ? 'bg-primary-100 text-primary' : ''
 						}`}
