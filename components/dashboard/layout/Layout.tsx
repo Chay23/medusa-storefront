786f4e5e6b287908176c9e7a0b78e7b3667ebedd
Nazar Shcherbii <shcherbiynazar@gmail.com>
2025-05-05 13:48:49 +0300

    Delete unused 'react-toastify' dependency

--- components/dashboard/layout/Layout.tsx
+++ components/dashboard/layout/Layout.tsx
@@ -3,7 +3,6 @@
 import type { ReactNode } from 'react';
 
 import Sidebar from '../UI/sidebar/Sidebar';
-import { ToastContainer } from 'react-toastify';
 import { Toaster } from 'react-hot-toast';
 
 export default function Layout({ children }: { children: ReactNode }) {
@@ -13,7 +12,6 @@ export default function Layout({ children }: { children: ReactNode }) {
 				<Sidebar />
 				<main className='p-9 w-full bg-background'>{children}</main>
 			</div>
-			{/* <ToastContainer position='bottom-center' hideProgressBar /> */}
 			<Toaster position='bottom-right' />
 		</>
 	);
