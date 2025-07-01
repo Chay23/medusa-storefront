fd1a8d2e734e7c270df92710ba72a790309abd4a
Nazar Shcherbii <shcherbiynazar@gmail.com>
2025-07-01 15:50:54 +0300

    Update spacing for UI elements

--- components/dashboard/collections/list/Collections.tsx
+++ components/dashboard/collections/list/Collections.tsx
@@ -7,11 +7,11 @@ import type { Breadcrumb } from '@/types/common/breadcrumbs';
 import { useState } from 'react';
 import { useModals } from '@/store/dashboard/modals';
 
-import { Button, Link } from '@heroui/react';
 import CollectionsFilters from './Filters';
 import CollectionsTable from './Table';
 import Breadcrumbs from '../../UI/breadcrumbs/Breadcrumbs';
 import DeleteCollectionModal from '../delete/DeleteCollectionModal';
+import CollectionsHeader from './CollectionsHeader';
 
 import { ID_COLLECTION_DELETE } from '@/lib/dashboard/constants';
 import { paths } from '@/config/paths';
@@ -44,16 +44,7 @@ export default function Collections({ collectionsRes }: Props) {
 			)}
 			<Breadcrumbs items={breadcrumbs} />
 			<section className='content-container'>
-				<div className='flex justify-between mb-7'>
-					<h2>Collections</h2>
-					<Button
-						as={Link}
-						href={paths.dashboard.createCollection.getHref()}
-						color='primary'
-					>
-						Create Collection
-					</Button>
-				</div>
+				<CollectionsHeader />
 				<CollectionsFilters />
 				<CollectionsTable
 					collectionsRes={collectionsRes}
