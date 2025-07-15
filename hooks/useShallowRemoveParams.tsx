f93f3ecf2b9cdb0b9dba7ed4c14d6250b0a5a056
Nazar Shcherbii <shcherbiynazar@gmail.com>
2025-07-15 14:29:31 +0300

    Add useShallowRemoveParams hook

---
+++ hooks/useShallowRemoveParams.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useShallowUpdateParams } from './useShallowUpdateParams';
+
+export default function useShallowRemoveParams(params: string[]) {
+	const { shallowUpdateParams } = useShallowUpdateParams();
+
+	useEffect(() => {
+		if (params.length > 0) {
+			shallowUpdateParams(
+				params.reduce((obj, param) => {
+					obj[param] = null;
+					return obj;
+				}, {} as Record<string, null>)
+			);
+		}
+	}, [shallowUpdateParams]);
+}
--- hooks/useShallowUpdateParams.tsx
+++ hooks/useShallowUpdateParams.tsx
@@ -1,15 +1,21 @@
+'use client';
+
 import { useSearchParams } from 'next/navigation';
+import { useCallback } from 'react';
 
 export function useShallowUpdateParams() {
 	const searchParams = useSearchParams();
 
-	const shallowUpdateParams = (updates: Record<string, string | null>) => {
-		const params = new URLSearchParams(searchParams);
-		Object.entries(updates).forEach(([key, value]) =>
-			value ? params.set(key, value) : params.delete(key)
-		);
-		window.history.pushState(null, '', `?${params}`);
-	};
+	const shallowUpdateParams = useCallback(
+		(updates: Record<string, string | null>) => {
+			const params = new URLSearchParams(searchParams);
+			Object.entries(updates).forEach(([key, value]) =>
+				value ? params.set(key, value) : params.delete(key)
+			);
+			window.history.pushState(null, '', `?${params}`);
+		},
+		[searchParams]
+	);
 
 	return {
 		shallowUpdateParams,
